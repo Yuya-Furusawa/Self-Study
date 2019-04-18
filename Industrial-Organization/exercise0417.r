5673912dcbd9579ab41808d5c627b497f148b625
Yuya Furusawa <fullyou0798@gmail.com>
2019-04-19 02:50:46 +0900

    Fix R Code of exercise0417

--- Industrial-Organization/exercise0417.r
+++ Industrial-Organization/exercise0417.r
@@ -6,22 +6,21 @@
 # y_i = \beta_0 + \beta_1^{x1_i} + \beta_2 * x2 + \epsilon
 
 data <- read.csv("DataProblem201904.csv", header=FALSE)
-f <- function (data){
-  y <- data[,1]
-  x1 <- data[,2]
-  x2 <- data[,3]
-  n <- length(y)
+
+f <- function (theta, data){
+    y <- data[,1]
+    x1 <- data[,2]
+    x2 <- data[,3]
+    n <- length(y)
   
-  return(function(theta){
     total = 0
     for (i in 1:n){
       total <- total + (y[i] - theta[1] - theta[2]^x1[i] - theta[3]*x2[i] )^2
     }
     return(total)
-  })
 }
 
-optim(par=c(1,1,1), fn=f(data))
+optim(par=c(1,1,1), fn=f, data=data)
 
 
 # Results

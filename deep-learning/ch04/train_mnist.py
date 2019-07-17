88ff7b11f8c1308461f65689a2988a7a6b298670
Yuya <fullyou0798@gmail.com>
2019-07-18 02:18:39 +0900

    evaluate each epoch

--- deep-learning/ch04/train_mnist.py
+++ deep-learning/ch04/train_mnist.py
@@ -70,11 +70,14 @@ class TwoLayerNet:
 	 load_mnist(normalize=True, one_hot_label=True)
 
 train_loss_list = []
+train_acc_list = []
+test_acc_list = []
 
 iters_num = 10000
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
+iter_per_epoch = max(train_size / batch_size, 1)
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
@@ -94,23 +97,9 @@ for i in range(iters_num):
 	loss = network.loss(x_batch, t_batch)
 	train_loss_list.append(loss)
 
-import matplotlib.pyplot as plt
-
-plt.plot(range(iters_num), train_loss_list)
-plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	if i % iter_per_epoch == 0:
+		train_acc = network.accuracy(x_train, t_train)
+		test_acc = network.accuracy(x_test, t_test)
+		train_acc_list.append(train_acc)
+		test_acc_list.append(test_acc)
+		print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
